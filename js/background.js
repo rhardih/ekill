f83420acb4b7a1d999616acfcbff17b91a759896
René Hansen <renehh@gmail.com>
2018-10-18 15:08:44 +0200

    Adds icon badge for new changes notification When a new version is rolled, ekill will show a little "New" badge atop the toolbar icon. When ekill is then activated subsequently, by either click or hotkey, instead of activating like it normally would, the user is taken to a Changelog page, with indications on what was been added since they last visited that page. Only the first activation will cause this page to open. Fixes #20

--- js/background.js
+++ js/background.js
@@ -1,5 +1,30 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {});
-  });
+chrome.storage.sync.get({
+  "ekillVersion": {
+    shownChangesFor: "0.0"
+  }
+}, item => {
+  if (chrome.runtime.lastError) {
+    console.error(chrome.runtime.lastError);
+  } else {
+    let ekillVersion = item.ekillVersion;
+    let stringVersion = chrome.runtime.getManifest().version.toString();
+    let showChanges = ekillVersion.shownChangesFor < stringVersion;
+
+    if (showChanges) {
+      chrome.browserAction.setBadgeBackgroundColor({ color: "#000000" });
+      chrome.browserAction.setBadgeText({text: "New"});
+    }
+
+    chrome.browserAction.onClicked.addListener(tab => {
+      if (showChanges) {
+        chrome.tabs.create({ url: chrome.extension.getURL("changelog.html") });
+
+        showChanges = false;
+        chrome.browserAction.setBadgeText({text: ""});
+
+      } else {
+        chrome.tabs.sendMessage(tab.id, {});
+      }
+    });
+  }
 });
