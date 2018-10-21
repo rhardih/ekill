fb19daa206a225fd8440fce4de5bb404e50c840e
René Hansen <renehh@gmail.com>
2018-10-21 09:51:06 +0200

    Scope chrome global in background.js

--- js/background.js
+++ js/background.js
@@ -1,30 +1,33 @@
-chrome.storage.sync.get({
-  "ekillVersion": {
-    shownChangesFor: "0.0"
-  }
-}, item => {
-  if (chrome.runtime.lastError) {
-    console.error(chrome.runtime.lastError);
-  } else {
-    let ekillVersion = item.ekillVersion;
-    let stringVersion = chrome.runtime.getManifest().version.toString();
-    let showChanges = ekillVersion.shownChangesFor < stringVersion;
+(c => {
+  c.storage.sync.get({
+    "ekillVersion": {
+      shownChangesFor: "0.0"
+    }
+  }, item => {
+    if (c.runtime.lastError) {
+      console.error(c.runtime.lastError);
+    } else {
+      let ekillVersion = item.ekillVersion;
+      let stringVersion = c.runtime.getManifest().version.toString();
+      let showChanges = ekillVersion.shownChangesFor < stringVersion;
 
-    if (showChanges) {
-      chrome.browserAction.setBadgeBackgroundColor({ color: "#000000" });
-      chrome.browserAction.setBadgeText({text: "New"});
+      if (showChanges) {
+        c.browserAction.setBadgeBackgroundColor({ color: "#000000" });
+        c.browserAction.setBadgeText({text: "New"});
+      }
+
+      c.browserAction.onClicked.addListener(tab => {
+        if (showChanges) {
+          c.tabs.create({ url: c.extension.getURL("changelog.html") });
+
+          showChanges = false;
+          c.browserAction.setBadgeText({text: ""});
+
+        } else {
+          c.tabs.sendMessage(tab.id, {});
+        }
+      });
     }
 
-    chrome.browserAction.onClicked.addListener(tab => {
-      if (showChanges) {
-        chrome.tabs.create({ url: chrome.extension.getURL("changelog.html") });
-
-        showChanges = false;
-        chrome.browserAction.setBadgeText({text: ""});
-
-      } else {
-        chrome.tabs.sendMessage(tab.id, {});
-      }
-    });
-  }
-});
+  });
+})(chrome);
